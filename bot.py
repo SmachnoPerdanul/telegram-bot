6578d445b2fc46bffce92bc16c65182f6728b95b
SmachnoPerdanul <perdanulsmachno@gmail.com>
2026-06-19 16:03:02 +0300

    Первая версия телеграм-бота на aiogram

---
+++ bot.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Привет! Я твой первый бот.")
+
+
+@dp.message()
+async def echo_handler(message: Message):
+    await message.answer(f"Ты написал: {message.text}")
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
