41efbb8a688c8a382a84149d773b177bd8c8b064
SmachnoPerdanul <perdanulsmachno@gmail.com>
2026-06-19 16:45:17 +0300

    Добавил обработку комманды /help

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from dotenv import load_dotenv
 
@@ -18,11 +18,13 @@ dp = Dispatcher()
 async def start_handler(message: Message):
     await message.answer("Привет! Я твой первый бот.")
 
+@dp.message(Command("help"))
+async def help_handler(message: Message):
+    await message.answer("Чем могу помочь?")
 
 @dp.message()
 async def echo_handler(message: Message):
     await message.answer(f"Ты написал: {message.text}")
-
 
 async def main():
     bot = Bot(token=TOKEN)
